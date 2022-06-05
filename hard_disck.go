6afd1c58d3a2ec0c17936fdffa429c67b5a78ab4
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-05 17:51:26 +0430

    comments

---
+++ hard_disck.go
@@ -0,0 +1,46 @@
+package main
+
+// more details :
+
+// In programming whenever such functions
+// Save to file
+// We were dealing with hardware, we also need to know how to work with standard libraries.
+
+// Check the standard package library in Go:
+// To check Go libraries
+// Go to your browser and enter the main site of Go.
+// In the Standard Library section, we see that there is a list of standard Go libraries.
+// For example, search for the following package in the search field:
+// Ioutil
+// It is a very small package that uses some common tasks to work with the hard disk and is used in a machine or computer or laptop and anything else that we write in it, we click on it to bring it from there In the case of packages search, several items are displayed. In this case, we open the package with the following description:
+// Package iout implements some i / o utility functions
+// After this, the relevant documents can be viewed.
+
+// In this section, if we scroll down, we will see the various variables and functions related to this standard library.
+
+// To check one of the functions of this section click on the function
+// Write file
+
+// Click.
+// Func write file
+// We can see the relevant documentation.
+
+// The Write file function is used to create the file on our local hard disk.
+
+// In the first part of this function, the following statement is written in parentheses of the relevant arguments;
+
+// Func write file (file name string, data [] byte, permos.filemode) error
+
+// The first argument; file name string
+// That is, the file name is a string type.
+
+// So this function expects us to call the name of a string file with the first argument we write.
+
+//  The second argument is the raw data we want to write to the hard disk.
+// Slice of byte
+// Wherever we see a bracket score, we are talking about slices.
+
+//  Third argument;
+// Perm os.filemode
+// Perm = short for permission
+// Tells us what permissions to use if we need to create a file.
