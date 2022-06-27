6663ce6772f176477578b6a2d3148721327d030e
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-27 17:14:06 +0430

    finish today tasks

---
+++ maintwo.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	cards := newdeck()
+	caeds.shuffle()
+	cards.print()
+}
+
+// output in terminal :
+// created a list of random cards and show it
+
+// each time we mix the deck of card , usually GO provided by this list unchanged means in same state but randomly
+
+rand : Intn(len(d) -1)
+// intn is random number generator
+// GO by default uses a way to generate random number
+// This method depends on parameters such as seed and its value.
+// seed is a source for generate random number
+// we import seed to generator numbers then generator create a set of random (numbers or any thing else)
