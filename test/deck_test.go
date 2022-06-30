a40d0324686ac3e1e693f9598c2d15b1dbaf5189
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-30 16:16:44 +0430

    finish testing

--- test/deck_test.go
+++ test/deck_test.go
@@ -1,18 +1,23 @@
 package main
-// run package tests
-// run file tests
-// run package test
-package main
-// test in GO help us to check that our project run correctly or not.
-// we want know how the testing works and make sure that result is that thing we excepted
-// check for newdeck 
-// about newdeck there are three points that if this points was ok means the testing is ok and the function works right
-// 1-each time a newdeck for each type of cards created , four state must be done (four string) ...for example spades , diamonds...
-// (the length of deck must be 4 )
-// 2-the order must be define for example usually the first card should be Ace
-// 3-again the order must be define for example usually the last card should be Four
 
-// for func test newdeck
-// 1-sode to make sure that a deck is created with x number of cards
-// 2-code to make sure that the first card as an Ace
-// 3- code to make sure that the last card is a Four of spades 
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+
+	// this function use for testing NewDeck
+	// automatically call by GO test runner , this parameter refer to an argument that we refer to it by T
+	d. := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+		// each newdeck has 4 suit and 4 value
+		// in the end must has 16 cards generated
+		// in this commands we said if the function length not equal to 16 a parameter went worng
+		// we notice this note to testhandler (responsible for testing)
+		// the text used for this reason
+		// the second argument show this authorizes number for client
+	}
+}
+
+// *testing.T is a test handler , verify the type of value that will send to the function
+
+// out put : test passed
