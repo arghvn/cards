33541fe52d1fbf6c4c59a2140ace0c729af13f08
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-06 18:24:07 +0430

    important comment

--- test/struct/struct3.go
+++ test/struct/struct3.go
@@ -19,9 +19,24 @@ func main() {
 			zipcode: 94000,
 		}
 	}
-	fmt.printf("%+v", Jim)
+	jim.print()
+	// fmt.printf("%+v", Jim)
 }
 
 // output:
 // firstname : jim lastname : party
 // contact: {email:jim@gmail.com and zipcode : 94000}
+
+
+// in deck application when we defined a new type of deck , some functions can be considered as a reciever
+// like cards.shuffle()
+// we can set same type by using struct(those have reciever)
+// so in this code we can define a person as a reciever 
+// we will define a function that recieve a person(like Jim)and do something like printing all persons details
+
+func (p person) print() {
+	fmt.printf("%v", p)
+}
+
+// above command means we can call this function with any type or any value related to person.
+
