dc2f0eb9227907c2088dedb0bb464556759cc299
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-11 12:42:30 +0430

    today task

---
+++ test/struct/iterate_over_maps.go
@@ -0,0 +1,15 @@
+// we want in this new project define a function that get map andd take iteration  over it
+// and then print key value pairs
+
+package main
+
+import "fmt"
+
+func main(){
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "4bf745",
+		"white": "#ffffff"
+	}
+	fmt.println(colors)
+}
