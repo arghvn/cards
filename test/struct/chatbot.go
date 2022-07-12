0c8b7548dac095873d0333c0e17c23203e02bfb9
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-12 10:45:56 +0430

    printgreetnig function

---
+++ test/struct/chatbot.go
@@ -0,0 +1,29 @@
+package main
+
+// in robot we must create struct and then getgreeting function and printgreeting function
+
+type EnglishBot struct{}
+type SpanishBot struct{}
+
+// struct{} is struct without any associated fields.
+
+// in struct we have to define the ability to recieve Englishbot and Spanishnot as a reciever
+
+func main() {
+
+}
+
+func (eb EnglishBot) getgreeting() string {
+	// very custom logic for generating an English greeting
+	return "Hi there!"
+}
+
+func (sb SpanishBot) getgreeting() string {
+	return "Hola!"
+}
+
+// in both function we see the reciever section that recieve eb or sb .
+
+// function getgreeting for english and spanish probably very different logic in these functions
+
+// function printgreeting ,this will probably have identical logic
