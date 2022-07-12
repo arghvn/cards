09c71ca7295645244f0be5d97168f1817e25abb8
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-07-12 09:00:17 +0430

    first output for hex

--- test/struct/newdeclaremap.go
+++ test/struct/newdeclaremap.go
@@ -18,3 +18,29 @@ func main () {
 
 // the third way is direct way 
 
+// consider printmap function and assign a argument to it (c) ant then define this argument to colormap
+
+type map
+type key string
+type value string
+
+func printmap(c map[string]string){
+	for color,hex := range c {
+    fmt.println("hex code for", color ,hex ,color ,"is" ,hex)
+	}
+	printmap(colors)
+}
+
+// in this function we use commands for iteration over map by use for key and then use two variables for recieve each key and value in loop
+// colors = key , hex = value
+
+// so to define GO that we want iterate over map and the for body length is constant ,we can use commands that be unique for key and value pair.
+
+// summary :
+// c is an argument ,color is key for this step through the loop ,hex is value for this step through the loop
+
+
+// output :
+// hex code for green is #45f745
+// hex code for white is #ffffff
+// hex code for red is #ff0000
